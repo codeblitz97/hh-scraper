b0b08e4d5b40a7c6b3474a8b0c87d7832f27c849
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-12-15 18:39:30 +0600

    Create recent.js

---
+++ scrape/recent.js
@@ -0,0 +1,66 @@
+const axios = require('axios').default;
+const uae = require('user-agent-array');
+const { load } = require('cheerio');
+const { BaseURL } = require('../config');
+
+const ua = uae[(Math.random() * uae.length) | 0];
+
+const getRecents = async (page) => {
+  let url;
+  let info = [];
+  const uniqueSet = new Set();
+
+  try {
+    if (page <= 1) {
+      url = `${BaseURL}/release/2023`;
+    } else {
+      url = `${BaseURL}/release/2023/page/${page}`;
+    }
+
+    const response = await axios.get(`${url}`, {
+      headers: { 'User-Agent': ua },
+    });
+    const data = await response.data;
+    const loadedCheerioData = load(data);
+    const element = loadedCheerioData(
+      'body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div.main-col.col-md-8.col-sm-8 > div > div.c-page > div > div.tab-content-wrap > div > div'
+    );
+    element.each((index, el) => {
+      const divs = loadedCheerioData(el).find('div');
+      divs.each((index, elm) => {
+        const title = loadedCheerioData(elm)
+          .find('div > div.page-item-detail > div > a')
+          .attr('title');
+        const watchId = loadedCheerioData(elm)
+          .find('div > div.page-item-detail > div > a')
+          .attr('href');
+        const id = watchId?.split('/watch/')[1].replace('/', '');
+
+        const image = loadedCheerioData(elm)
+          .find('div > div.page-item-detail > div > a > img')
+          .attr('src')
+          ?.replace(/\s/g, '%20');
+
+        if (title && image && id) {
+          const obj = {
+            id: id,
+            title: title,
+            image: image,
+            url: watchId,
+          };
+
+          if (!uniqueSet.has(obj.id)) {
+            uniqueSet.add(obj.id);
+            info.push(obj);
+          }
+        }
+      });
+    });
+    return info;
+  } catch (error) {
+    console.error(error);
+    return { message: '' };
+  }
+};
+
+module.exports = { getRecents };
