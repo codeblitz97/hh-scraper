428abcf2f339de5c77a965cbc18cb1ae40f927b6
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-12-17 15:18:32 +0600

    Update error middleware to log the error

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  console.log(err);
   res
     .status(err.status || 500)
     .json({ error: err.message || 'Internal Server Error' });
