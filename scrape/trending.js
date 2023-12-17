d83e1d8c13802cd36b669d5abc286bbc9fd66824
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-12-17 15:19:41 +0600

    Update the trending scraper

--- scrape/trending.js
+++ scrape/trending.js
@@ -1,54 +1,78 @@
-const puppeteer = require('puppeteer');
-const { BaseURL } = require('../config');
+const axios = require('axios').default;
+const { load } = require('cheerio');
 const uae = require('user-agent-array');
 const createError = require('http-errors');
+const { BaseURL } = require('../config');
 
 const ua = uae[(Math.random() * uae.length) | 0];
 
-async function getTrendingMonth() {
+const getTrending = async (page = 1, limit = 10, type = 'monthly') => {
   try {
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
-    const customUserAgent = ua;
-    await page.setUserAgent(customUserAgent);
-    await page.goto(BaseURL);
+    let uniqueSet = new Set();
+    let trendingArr = [];
+    let url = '';
+    const startIndex = (page - 1) * limit;
 
-    await page.waitForSelector(
-      'body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div.main-col.col-md-8.col-sm-8 > div > div.c-page > div > div > div > div > div:nth-child(2) > div.page-content-listing.item-big_thumbnail > div > div > div > div.owl-stage-outer > div > div.owl-item.active',
-      { timeout: 7000 }
-    );
+    if (page <= 1 && type.toLowerCase() === 'monthly') {
+      url = `${BaseURL}/browse/trending`;
+    } else if (page > 1 && type.toLowerCase() === 'monhtly') {
+      url = `${BaseURL}/browse/trending/page/${page}`;
+    } else if (page <= 1 && type.toLowerCase() === 'all_time') {
+      url = `${BaseURL}/browse/trending/?order=all`;
+    } else if (page > 1 && type.toLowerCase() === 'all_time') {
+      url = `${BaseURL}/browse/trending/page/${page}/?order=all`;
+    } else {
+      url = `${BaseURL}/browse/trending/`;
+    }
 
-    const data = await page.evaluate(() => {
-      const items = document.querySelectorAll(
-        'body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div.main-col.col-md-8.col-sm-8 > div > div.c-page > div > div > div > div > div:nth-child(2) > div.page-content-listing.item-big_thumbnail > div > div > div > div.owl-stage-outer > div > div.owl-item.active'
-      );
-      const scrapedData = [];
-
-      items.forEach((item) => {
-        const title = item.querySelector('.post-title a').textContent.trim();
-        const rating = item
-          .querySelector('.post-total-rating .score')
-          .textContent.trim();
-        const image = item.querySelector('.item-thumb img').getAttribute('src');
-        const href = item.querySelector('.post-title a').getAttribute('href');
-
-        scrapedData.push({
-          title,
-          rating,
-          image,
-          href,
-        });
-      });
-
-      return scrapedData;
+    const response = await axios.get(url, {
+      headers: { 'User-Agent': ua },
     });
 
-    await browser.close();
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw createError(500, 'Internal Server Error');
-  }
-}
+    if (!response || !response.data) {
+      throw createError(500, 'Failed to fetch data');
+    }
 
-module.exports = { getTrendingMonth };
+    const data = await response.data;
+    const loadedCheerioData = load(data);
+    const element = loadedCheerioData(
+      'body > div.wrap > div > div > div.browse > div'
+    );
+
+    element.each((index, el) => {
+      const divs = loadedCheerioData(el).find('div:nth-child(3) > div');
+      divs.each((index, elm) => {
+        const title = loadedCheerioData(elm).find('.h_cover > a').attr('title');
+        const watchId = loadedCheerioData(elm)
+          .find('.h_cover > a')
+          .attr('href');
+        const id = watchId?.split('/watch/')[1].replace('/', '');
+
+        const image = loadedCheerioData(elm)
+          .find('.h_cover > a > img')
+          .attr('src')
+          ?.replace(/\s/g, '%20');
+
+        if (title && image && id) {
+          const obj = {
+            id: id,
+            title: title,
+            image: image,
+            url: watchId,
+          };
+
+          if (!uniqueSet.has(obj.id) && trendingArr.length < limit) {
+            uniqueSet.add(obj.id);
+            trendingArr.push(obj);
+          }
+        }
+      });
+    });
+
+    return trendingArr;
+  } catch (error) {
+    throw createError(500, error?.message);
+  }
+};
+
+module.exports = { getTrending };
